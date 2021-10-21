3e84ac04761b009895c2d3500df42d57617f82c5
Nco9r <nicolas929@me.com>
2021-10-21 11:49:41 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+
+    <nuxt />
+
+  </div>
+</template>
+<script>
+
+export default {
+  components: {
+ 
+  },
+}
+</script>
+
+<style>
+html {
+  font-family: lato, Times, serif, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  overflow-x: hidden;
+}
+
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+body {
+    overflow-x: hidden;
+}
+:root {
+  --background: #F8F2E7;
+  --redBody: #9c3135; 
+  --green: #3e834e; 
+  --green-rhum: #155323; 
+  --body: #4e4e4e;
+  --white: #fff;
+  --black: #202020;
+}
+
+
+
+
+
+
+
+
+</style>
